35038ce423de8b1b65b72a214b1f815fcdd59663
Daniel Heidemann <mail@dheidemann.de>
2024-02-10 11:09:05 +0100

    graphql and python venv for future implementation of anki colpkg converter

--- lib/modules/deck_selection_menu.dart
+++ lib/modules/deck_selection_menu.dart
@@ -1,75 +1,72 @@
 import 'package:cards/views/colored_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:cards/constants.dart' as c;
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class DeckSelectionMenu extends StatelessWidget {
-  final List<Map<String, dynamic>> deckData;
-  const DeckSelectionMenu({required this.deckData, super.key});
+  const DeckSelectionMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("dibsel"),
-      ),
-      body: SingleChildScrollView(
-        child: DataTable(
-          columns: const <DataColumn>[
-            DataColumn(
-              label: Expanded(
-                child: Text(
-                  'Modul',
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-            DataColumn(
-              label: Expanded(
-                child: Text(
-                  'Prof.',
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-            DataColumn(
-              label: Expanded(
-                child: Text(
-                  'Semester',
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-            DataColumn(
-              label: Expanded(
-                child: Text(
-                  'Karten',
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-            DataColumn(
-              label: Expanded(
-                child: Text(
-                  'Optionen',
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-          ],
-          rows: deckData.map((deck) {
-            return DataRow(cells: [
-              DataCell(Text(deck["module"].toString())),
-              DataCell(Text(deck["prof"].toString())),
-              DataCell(Text(deck["semester"].toString())),
-              DataCell(Text(deck["size"].toString())),
-              DataCell(Row(
-                children: [
-                  ColoredIconButton(Icon(Icons.download), c.Colors.orange)
-                ],
-              ))
-            ]);
-          }).toList(),
+        appBar: AppBar(
+          title: const Text("dibsel"),
         ),
+        body: Query(
+            options: QueryOptions(document: gql(c.GraphQL.fetchDecks)),
+            builder: (result, {fetchMore, refetch}) {
+              if (result.isLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              if (result.data == null) {
+                return const Center(
+                  child: Text(c.Strings.nothingFound),
+                );
+              }
+
+              final decks = result.data!["decks"];
+              return SingleChildScrollView(
+                child: DataTable(
+                  columns: const <DataColumn>[
+                    DataColumn(label: DeckTableTitle(text: c.Strings.module)),
+                    DataColumn(label: DeckTableTitle(text: c.Strings.prof)),
+                    DataColumn(label: DeckTableTitle(text: c.Strings.semester)),
+                    DataColumn(label: DeckTableTitle(text: c.Strings.size)),
+                    DataColumn(label: DeckTableTitle(text: c.Strings.options)),
+                  ],
+                  rows: decks.map((deck) {
+                    return DataRow(cells: [
+                      DataCell(Text(deck["module"].toString())),
+                      DataCell(Text(deck["prof"].toString())),
+                      DataCell(Text(deck["semester"].toString())),
+                      DataCell(Text(deck["size"].toString())),
+                      DataCell(Row(
+                        children: [
+                          ColoredIconButton(
+                              Icon(Icons.download), c.Colors.orange)
+                        ],
+                      ))
+                    ]);
+                  }).toList(),
+                ),
+              );
+            }));
+  }
+}
+
+class DeckTableTitle extends StatelessWidget {
+  final String text;
+  const DeckTableTitle({required this.text, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text(
+        text,
+        style: const TextStyle(fontStyle: FontStyle.italic),
       ),
     );
   }
