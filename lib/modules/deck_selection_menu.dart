948ef53be57d85bd65c2e36a61534543590c4c82
Daniel Heidemann <mail@dheidemann.de>
2024-02-11 18:25:20 +0100

    finally a working database connection

--- lib/modules/deck_selection_menu.dart
+++ lib/modules/deck_selection_menu.dart
@@ -9,51 +9,83 @@ class DeckSelectionMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("dibsel"),
-        ),
-        body: Query(
-            options: QueryOptions(document: gql(c.GraphQL.fetchDecks)),
-            builder: (result, {fetchMore, refetch}) {
-              if (result.isLoading) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
+    return SelectionArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text("dibsel"),
+          ),
+          body: Query(
+              options: QueryOptions(document: gql(c.GraphQL.fetchDecks)),
+              builder: (QueryResult result, {fetchMore, refetch}) {
+                if (result.isLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
 
-              if (result.data == null) {
-                return const Center(
-                  child: Text(c.Strings.nothingFound),
-                );
-              }
+                if (result.hasException) {
+                  return const Center(
+                    child: Text(c.Strings.error),
+                  );
+                }
 
-              final decks = result.data!["decks"];
-              return SingleChildScrollView(
-                child: DataTable(
-                  columns: const <DataColumn>[
-                    DataColumn(label: DeckTableTitle(text: c.Strings.module)),
-                    DataColumn(label: DeckTableTitle(text: c.Strings.prof)),
-                    DataColumn(label: DeckTableTitle(text: c.Strings.semester)),
-                    DataColumn(label: DeckTableTitle(text: c.Strings.size)),
-                    DataColumn(label: DeckTableTitle(text: c.Strings.options)),
-                  ],
-                  rows: decks.map((deck) {
-                    return DataRow(cells: [
-                      DataCell(Text(deck["module"].toString())),
-                      DataCell(Text(deck["prof"].toString())),
-                      DataCell(Text(deck["semester"].toString())),
-                      DataCell(Text(deck["size"].toString())),
-                      DataCell(Row(
-                        children: [
-                          ColoredIconButton(
-                              Icon(Icons.download), c.Colors.orange, () {})
-                        ],
-                      ))
-                    ]);
-                  }).toList(),
-                ),
-              );
-            }));
+                final List<Map<String, dynamic>> decks =
+                    List<Map<String, dynamic>>.from(result.data!["decks"]);
+
+                if (decks.isEmpty) {
+                  return const Center(
+                    child: Text(c.Strings.nothingFound),
+                  );
+                }
+
+                return SingleChildScrollView(
+                  child: DataTable(
+                    columns: const <DataColumn>[
+                      DataColumn(label: DeckTableTitle(text: c.Strings.module)),
+                      DataColumn(label: DeckTableTitle(text: c.Strings.prof)),
+                      DataColumn(
+                          label: DeckTableTitle(text: c.Strings.semester)),
+                      DataColumn(label: DeckTableTitle(text: c.Strings.size)),
+                      DataColumn(
+                          label: DeckTableTitle(text: c.Strings.language)),
+                      DataColumn(
+                          label: DeckTableTitle(text: c.Strings.options)),
+                    ],
+                    rows: decks
+                        .map((deck) => DataRow(cells: [
+                              DataCell(Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(deck["module"]?.toString() ?? ""),
+                                  if (deck["moduleAlt"] != null) ...[
+                                    Text(
+                                      deck["moduleAlt"].toString(),
+                                      style: const TextStyle(
+                                          color: Colors.grey, fontSize: 12),
+                                    ),
+                                  ]
+                                ],
+                              )),
+                              DataCell(
+                                  Text(deck["examiners"]?.toString() ?? "")),
+                              DataCell(Text(
+                                  (deck["semester"]?.toString() ?? "") +
+                                      (deck["year"]?.toString() ?? ""))),
+                              DataCell(Text(deck["size"]?.toString() ?? "")),
+                              DataCell(
+                                  Text(deck["language"]?.toString() ?? "")),
+                              DataCell(Row(
+                                children: [
+                                  ColoredIconButton(Icon(Icons.download),
+                                      c.Colors.orange, () {})
+                                ],
+                              ))
+                            ]))
+                        .toList(),
+                  ),
+                );
+              })),
+    );
   }
 }
