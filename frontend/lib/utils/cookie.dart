d0f193eddc964d48b64c1f1e7b248ef2dc914ca3
Daniel Heidemann <mail@dheidemann.de>
2024-03-01 17:10:08 +0100

    better cookie helper function order

---
+++ frontend/lib/utils/cookie.dart
@@ -0,0 +1,63 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+import 'dart:convert';
+import 'package:cards/main.dart';
+import 'package:flutter/material.dart';
+import 'package:jwt_decode/jwt_decode.dart';
+
+String? getValueOfCookie(String key) {
+  String? cookieString = html.document.cookie;
+  if (cookieString == null) {
+    logflob.shout("Failed to get cookies");
+    return null;
+  }
+
+  List<String> cookies = cookieString.split("; ");
+  for (String cookie in cookies) {
+    List<String> parts = cookie.split("=");
+    if (parts[0] == key) {
+      return parts[1];
+    }
+  }
+
+  return null;
+}
+
+Map<String, dynamic>? getDecodedToken() {
+  final jwtToken = getValueOfCookie("jwt");
+
+  if (jwtToken != null) {
+    Map<String, dynamic> decodedToken = Jwt.parseJwt(jwtToken);
+    decodedToken["user"] = jsonDecode(decodedToken["user"])["username"];
+    decodedToken["jwt"] = jwtToken;
+    logflob.shout("token: $decodedToken");
+    return decodedToken;
+  }
+
+  return null;
+}
+
+void setCookie(String cookie) {
+  try {
+    html.document.cookie = cookie;
+  } catch (e) {
+    logflob.shout("error during setting cookie: $e");
+  }
+}
+
+void setTokenCookieFromUrl() {
+  final url = Uri.base;
+  final queryParams = url.queryParameters;
+  var token = queryParams["token"];
+
+  if (token != null) {
+    setCookie("jwt=$token; Path:/; SameSite=None");
+  }
+}
+
+void setThemeCookie() {
+  if (getValueOfCookie("theme") == null) {
+    String theme = ThemeMode.system == ThemeMode.dark ? "dark" : "light";
+    setCookie("theme=$theme");
+  }
+}
--- frontend/lib/utils/login.dart
+++ frontend/lib/utils/login.dart
@@ -3,16 +3,6 @@ import 'package:cards/constants.dart' as c;
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
-void setCookieFromUrl() {
-  final url = Uri.base;
-  final queryParams = url.queryParameters;
-  var token = queryParams["token"];
-
-  if (token != null) {
-    html.document.cookie = "jwt=$token; Path:/; SameSite=None";
-  }
-}
-
 Future<void> login() async {
   try {
     html.window.location.href = c.GraphQL.loginUrl;
